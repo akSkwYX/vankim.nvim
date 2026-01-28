ad602a0d9c5cde33d1dd418fdd270713bc72735f
akSkwYX <akskwyx@gmail.com>
2026-01-28 23:03:59 +0100

    Will too

--- grammar/grammar.js
+++ grammar/grammar.js
@@ -17,20 +17,20 @@ module.exports = grammar({
       field: $ => seq(
          field('name', token(/[^\n:]+/)),
          ':',
-         optional(field('body', repeat(choice($.typst_block, $.latex_block, $.text_line, $.blank_line))))
+         optional(field('body', repeat(choice($.typst_block, $.latex_block, $.text))))
       ),
 
-      text_line: $ => token(/(?:[^\[\n]|\[(?!\/?(typst|latex)\]))+\n?/),
+      text: $ => token(/[^[]+/),
 
       typst_block: $ => seq(
          token('[typst]'),
-         field('content', token(/[\s\S]*?(?=\[\/typst\])/)),
+         repeat(choice($.text)),
          token('[/typst]')
       ),
 
       latex_block: $ => seq(
          token('[latex]'),
-         field('content', token(/[\s\S]*?(?=\[\/latex\])/)),
+         repeat(choice($.text)),
          token('[/latex]')
       ),
    }
